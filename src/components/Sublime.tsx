f94832e0d8f2069ddd0e80c2da0ec056ebfb2714
Dio Deska <diodexa@gmail.com>
2025-12-07 15:31:18 +0700

    fixing button download sublime
    fixing button download

--- src/components/Sublime.tsx
+++ src/components/Sublime.tsx
@@ -5,7 +5,7 @@ return (
     <div className="absolute right-0 bottom-0">
         <a
             href="/sublime_text_build_4126.zip"
-            download
+            download="sublime_text_build_4126.zip"
             className="text-black bg-white border-2 border-solid pointer"
         >
             Download Notepad
